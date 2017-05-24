446cb0f48b340811815a229b32da6f28299f3938
Nick Dollimount <nick@nickdollimount.com>
2017-05-24 20:55:17 -0300

    Test-Port.ps1
    Alternative to using Telnet for testing port connectivity.

---
+++ Test-Port.ps1
@@ -0,0 +1,34 @@
+﻿function Test-Port
+{
+    Param(
+        [parameter(ParameterSetName='ComputerName', Position=0)]
+        [string]
+        $ComputerName,
+
+        [parameter(ParameterSetName='IP', Position=0)]
+        [System.Net.IPAddress]
+        $IPAddress,
+
+        [parameter(Mandatory=$true , Position=1)]
+        [int]
+        $Port
+        )
+
+    $RemoteServer = If ([string]::IsNullOrEmpty($ComputerName)) {$IPAddress} Else {$ComputerName};
+
+    $test = New-Object System.Net.Sockets.TcpClient;
+    Try
+    {
+        Write-Host "Connecting to "$RemoteServer":"$Port" (TCP)..";
+        $test.Connect($RemoteServer, $Port);
+        Write-Host "Connection successful";
+    }
+    Catch
+    {
+        Write-Host "Connection failed";
+    }
+    Finally
+    {
+        $test.Dispose();
+    }
+}
